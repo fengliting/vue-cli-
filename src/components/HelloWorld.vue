74c37fa407dec04e4d850b6ac54a2da09339bf83
flting <feng.liting@thebizark.com>
2018-10-16 16:47:35 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div>
+    <editor-bar v-model="editor.info" :isClear="isClear" @change="change"></editor-bar>
+  </div>
+</template>
+
+<script>
+
+import EditorBar from '@/components/EditorBar'
+export default {
+  data () {
+    return {
+      editor: {
+        info: ''
+      },
+      isClear: false
+    }
+  },
+  methods: {
+    change (val) {
+      this.editor.info1 = val
+    }
+  },
+  components: {
+    EditorBar
+  }
+}
+</script>
